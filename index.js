a80c6f205a4421c79fcec94fcab3c6b94f4b8eb9
Abby Anderson <abby.e.anderson@gmail.com>
2021-08-30 15:55:50 -0600

    commit tree 898c0b531430204552ab5b84206226b0407c56ee

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feetDistance){
   // Write your code here!
+  if (feetDistance <= 400) {
+    return "This one is on me!"
+  }
+  else if (feetDistance > 2000 && feetDistance < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (feetDistance > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+switch (tipAmount) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  default:
+    return 'Bye.';
+}
 }
